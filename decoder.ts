eff8b2adf87eff65d1ea7f083515ad2d8a8c1d2d
Christopher Shelley <dont@forgetmy.email>
2021-02-15 12:26:31 +0100

    WIP decoding recursive doodah

--- decoder.ts
+++ decoder.ts
@@ -7,8 +7,6 @@ export default function decode(encodedSymbols) {
   const sym = encodedSymbols.find((s) => s.degree === 1);
   const K = sym.K;
   const sourcePacketIndex = selectNeighbours(sym.index, sym.degree, sym.K);
-  console.log({ sourcePacketIndex });
-  console.log(encodedSymbols.map((es) => es.data.length));
   const decodedSymbols = [];
 
   // TODO: do any degree>1 encoding symbols XOR with source symbol with degree=1
@@ -17,7 +15,6 @@ export default function decode(encodedSymbols) {
   //   1,
   //   Math.floor(K * 1.5)
   // );
-  // console.log(neighboursOfDegreeOneSourceSymbol);
   let inc = 0;
   while (false) {
     //decodedSymbols.length < K) {
@@ -39,17 +36,130 @@ export default function decode(encodedSymbols) {
     { id: 6, neighbours: [1, 2], data: "test test test 5" },
   ];
 
-  function traverse(id, data) {
-    console.log(id);
-    if (data.length >= tree.length) return data;
-    const currentNode = tree.find((n) => n.id === id);
-    const asIds = data.map((e) => e);
+  // we need to "rebuild the encodedGraph"
+  // in each recursion, XOR things that have only 1 un-XOR-ed neigbour
+  // to start, let's console.log the neighbours
 
-    return currentNode.neighbours.map((nextId) =>
-      traverse(nextId, [...data, id])
-    );
+  // PYTHON CODE
+  // def recover_graph(symbols, blocks_quantity):
+  // """ Get back the same random indexes (or neighbors), thanks to the symbol id as seed.
+  // For an easy implementation purpose, we register the indexes as property of the Symbols objects.
+  // """
+  // for symbol in symbols:
+
+  //     neighbors, deg = generate_indexes(symbol.index, symbol.degree, blocks_quantity)
+  //     symbol.neighbors = {x for x in neighbors}
+  //     symbol.degree = deg
+  // return symbols
+
+  const ax = (a, b) => {
+    const res = [];
+    for (let j = 0; j < a.length; j++) {
+      res[j] = a[j] ^ b[j];
+    }
+    return res;
+  };
+
+  let encodedGraph = encodedSymbols.map((s) => ({
+    eid: s.index,
+    nids: selectNeighbours(s.index, s.degree, s.K),
+    data: s.data,
+    degree: s.degree,
+  }));
+  console.log(
+    JSON.stringify(
+      encodedGraph.map((e) => ({ eid: e.eid, nids: e.nids })),
+      null,
+      4
+    )
+  );
+  const sourceGraph = [];
+
+  //TODO: SID and EIDS are fuck
+  let loop = 0;
+  function traverse(eid) {
+    // we traverse to an encoded symbol which hopefully has 1 degree remaining
+    loop++;
+    if (loop > 10) return;
+    console.log("traverse was called", eid);
+    const encodedSymbol = encodedGraph.find((s) => s.eid === eid);
+    if (encodedSymbol.nids.length > 1) {
+      // if this encoded symbol isnt ready to decode, we skip it
+      const next = encodedGraph.find((s) => s.degree === 1);
+      if (!next) {
+        console.log("FAILED - no remaining encoded symbols with degree of one");
+      } else {
+        console.log("DDDD");
+        traverse(next.nids[0]);
+      }
+    } else {
+      const sourceSymbol = sourceGraph.find(
+        (s) => s.sid === encodedSymbol.nids[0]
+      );
+      if (!sourceSymbol) {
+        // we're ready to decode here - theres only 1 SID and it hasnt been decoded yet.
+        const decodedSymbol = Object.assign(encodedSymbols)
+        decodedSymbol.sid = encodedSymbol.nids[0];
+        sourceGraph.push(decodedSymbol);
+
+        // now we update neighbours with the same SID in their nids array
+        const neighbours = encodedGraph.filter((neighbour) =>
+          neighbour.nids.includes(decodedSymbol.sid)
+        );
+      }
+    }
+        const neighbours = encodedGraph.filter((oes) =>
+          oes.nids.includes(symbol.sid)
+        );
+        // Xor symbol.data from each one
+        neighbours.forEach((n) => {
+          let updatedNeighbour = n;
+          console.log("before changes", n.eid, n.nids);
+          updatedNeighbour.data = ax(symbol.data, updatedNeighbour.data);
+          updatedNeighbour.nids = n.nids.filter((id) => id !== sid);
+          updatedNeighbour.degree = n.degree - 1;
+          encodedGraph = encodedGraph.map((es) =>
+            es.eid === n.eid ? updatedNeighbour : es
+          );
+          console.log("after changes", n.eid, n.nids);
+          if (updatedNeighbour.nids.length === 1) {
+            console.log("AAAA");
+            traverse(n.nids[0]);
+          } else {
+            const next = encodedGraph.find((s) => s.degree === 1);
+            if (!next) {
+              console.log(
+                "FAILED - no remaining encoded symbols with degree of one"
+              );
+            } else {
+              console.log("BBBB");
+              traverse(next.nids[0]);
+            }
+          }
+        });
+      }
+    }
+    if (sourceGraph.length === K) {
+      console.log("FINISHED!");
+    } else {
+      const next = encodedGraph.find((s) => s.degree === 1);
+      if (!next) {
+        console.log("FAILED - no remaining encoded symbols with degree of one");
+      } else {
+        console.log("CCCC");
+        traverse(next.nids[0]);
+      }
+    }
   }
-  const result = traverse(4, []);
-  console.table(JSON.stringify(result));
-  console.log(treeify.asTree(result, true));
+  //START
+  const firstSymbol = encodedGraph.find((s) => s.degree === 1);
+  if (!firstSymbol) {
+    console.log(
+      "FAIL, no encoded symbol with one degree. Cannot decode. Get more packets!"
+    );
+  } else {
+    console.log("first symbol found, decoding..");
+    traverse(firstSymbol.eid);
+    // console.log(sourceGraph);
+  }
 }
