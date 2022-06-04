a363e839fb110203002c71c92f234a0ad832df9e
Christopher Shelley <dont@forgetmy.email>
2022-06-04 11:23:24 +0200

    rm colors package, fix project name in console log

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 import encode from "./encoder";
 import decode from "./decoder";
-var colors = require("colors/safe");
 
 if (require.main === module) {
   const file = process.argv[2];
   const strategy = process.argv[3] || "robust"; // default is robust LT , but also systemic, ideal, raptor etc.
-  console.log(colors.green("LTJS running in command line"));
+  console.log("luby running");
   if (!file) {
     console.error("you need to provide a filepath");
   } else {
