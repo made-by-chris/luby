cf5ca2c95eb06c272a88f5072fcc7d260ca649fc
Christopher Shelley <dont@forgetmy.email>
2023-06-05 18:16:39 +0200

    name

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ if(typeof process !== 'undefined' && process.versions.node) {
   if (require.main === module) {
     const file = process.argv[2];
     const strategy = process.argv[3] || "robust"; // default is robust LT , but also systemic, ideal, raptor etc.
-    console.log("LTJS running in command line");
+    console.log("luby running in command line");
     if (!file) {
       console.error("you need to provide a filepath");
     } else {
